0f38dd4d8abfc8ed8f8714e0f72db95e950ad1ed
savvidis4 <charissavvidis4@gmail.com>
2026-02-13 18:10:49 +0200

    forgot password done

--- backend/app/routes/forgot_password_route.py
+++ backend/app/routes/forgot_password_route.py
@@ -5,19 +5,15 @@ from app.business.forgot_password_service import ForgotPasswordService
  
 forgot_password_bp = Blueprint('forgot_password', __name__)
 
-@forgot_password_bp.route('/forgot_password', methods=['POST'])
+@forgot_password_bp.route('/forgot_password', methods=['POST', 'GET'])
 @jwt_required()
 def forgot_password():
 
     data = request.get_json()
     current_user_id = int(get_jwt_identity())
-    new_pin = data.get('new_pin')
-    new_ver_pin = data.get('new_pin_verification')
 
-    forgot_password_service = ForgotPasswordService(db.session, current_user_id)
-
-    result = forgot_password_service.pin_change(current_user_id, new_pin, new_ver_pin)
-
-    return jsonify(result)
+    return jsonify({"success": True, "message": "Continue"})
+   
+        
 
 
